12ac104f1bccbcaa7a0056740119b0e6b36fc286
Mayank Gupta <mayankgupta20231@gmail.com>
2024-03-06 21:13:27 +0530

    Add Inter font family and import Google Fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,35 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import phn1 from "../assets/phn1.jpg"
+import phn2 from "../assets/phn2.jpeg"
+import phn3 from "../assets/phn3.jpeg"
+import phn4 from "../assets/phn4.jpeg"
+import phn5 from "../assets/phn5.jpeg"
+import phn6 from "../assets/phn6.jpeg"
+import phn7 from "../assets/phn7.jpeg"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
+
+
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div style={{ backgroundColor: '#f1faff' }} className="relative h-screen flex items-center gap-4 md:gap-2 flex-col justify-center font-sans">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <h1 className="font-inter text-center w-80 text-3xl font-semibold md:w-1/2 md:text-4xl lg:w-1/3">Why Apple is Best Place to Buy iPhone</h1>
+      <p className="font-inter text-center w-80 md:w-1/2 md:text-xl lg:w-1/3">
+        You can choose payment option that works for you, pay less with a trade-in, connect your new iPhone to your carrier, and get set
+        up quickly. You can also chat with a Specialist anytime. Learn more about your options.
+      </p>
+      <button className="font-inter border-2 py-2 px-4 shadow-lg mt-2">Learn More </button>
+      <img className="hidden absolute left-20" src={phn1.src} />
+      <img className="hidden absolute right-2 lg:inline lg:w-64 lg:h-40 xl:w-72 xl:right-28 2xl:right-40" src={phn2.src} />
+      <img className="hidden absolute left-2 lg:inline lg:w-64 lg:h-40 xl:w-72 xl:left-28 2xl:left-40" src={phn3.src} />
+      <img className="hidden absolute min-[400px]:left-10  min-[400px]:top-0  min-[400px]:h-40 min-[400px]:w-40 min-[400px]:inline md:top-20 xl:w-72 xl:h-44 lg:top-8" src={phn4.src} />
+      <img className="hidden absolute min-[400px]:right-10 min-[400px]:top-0 min-[400px]:h-40 min-[400px]:w-40 min-[400px]:inline md:top-20 xl:w-72 xl:h-44 lg:top-8" src={phn5.src} />
+      <img className="absolute bottom-0 h-40 w-64 min-[400px]:right-10 min-[400px]:w-40 md:bottom-20 xl:w-72 xl:h-44 lg:bottom-8" src={phn6.src} />
+      <img className="absolute bottom-0 h-40 w-64 min-[400px]:left-10 min-[400px]:w-40  md:bottom-20 xl:w-72 xl:h-44 lg:bottom-8" src={phn7.src} />
+    </div >
   );
 }
